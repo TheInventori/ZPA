6d94af206bb9cc02c67c1c829354c1b1ee7e02e9
TheInventori <148151506+TheInventori@users.noreply.github.com>
2024-09-23 09:28:48 +0200

    Delete 2024-09-23 directory

--- 2024-09-23/2/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    Trojuholnik test1 = new Trojuholnik();
-    test1.info();
-
-    Trojuholnik test2 = new Trojuholnik(7, 6);
-    test2.info();
-  }
-}
